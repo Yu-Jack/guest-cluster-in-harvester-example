b1da7c6154961c6030b4f4170721fb2d5094c286
Jack Yu <jack.yu@suse.com>
2023-12-13 15:24:15 +0800

    fmt

--- main.tf
+++ main.tf
@@ -1,25 +1,24 @@
 data "rancher2_cluster_v2" "rancher_cluster" {
-  name = "local"
+  name            = "local"
   fleet_namespace = "fleet-local"
 }
 
 data "rancher2_cluster_v2" "harvesterkvm" {
-  name = "kvm"
+  name            = "kvm"
   fleet_namespace = "fleet-default"
 }
 
-module "guest_cluster"  {
+module "guest_cluster" {
   providers = {
     rancher2 = rancher2
   }
 
   source = "./modules/guest_cluster"
 
-  rancher_api_url                 = var.rancher_api_url
-  rancher_access_key              = var.rancher_access_key
-  rancher_secret_key              = var.rancher_secret_key
-
-  cluster_name = "local-test"
-  rke2_version = "v1.26.10+rke2r2"
+  rancher_api_url               = var.rancher_api_url
+  rancher_access_key            = var.rancher_access_key
+  rancher_secret_key            = var.rancher_secret_key
+  cluster_name                  = "local-test"
+  rke2_version                  = "v1.26.11+rke2r1"
   harvester_cloud_credential_id = rancher2_cloud_credential.harvesterkvm.id
 }
