ba58d5b69e32bab7d6886b08dfabca981872fbfa
Cooper Tseng <cooper.tseng@suse.com>
2024-03-12 15:00:26 +0800

    Use different machine config in machine pool

--- modules/guest_cluster_harvester_cloud_provider/machine.tf
+++ modules/guest_cluster_harvester_cloud_provider/machine.tf
@@ -4,8 +4,54 @@ data "local_file" "cloud_init" {
 }
 
 # Create a new rancher2 machine config v2 using harvester node_driver
-resource "rancher2_machine_config_v2" "harvesterkvm" {
-  generate_name = "harvesterkvm"
+resource "rancher2_machine_config_v2" "controlplane" {
+  generate_name = "controlplane"
+  harvester_config {
+    vm_namespace = "default"
+    cpu_count = "2"
+    memory_size = "4"
+    disk_info = <<EOF
+    {
+        "disks": [{
+            "imageName": "${var.image_name}", 
+            "size": 15,
+            "bootOrder": 1
+        }]
+    }
+    EOF
+    network_info = <<EOF
+    {
+        "interfaces": [{
+            "networkName": "${var.network_name}"
+        }]
+    }
+    EOF
+    ssh_user = "ubuntu"
+    user_data = <<EOF
+    package_update: true
+    # This is for reinstalling harvester cloud provider.
+    # If you don't need to reinstall, you can remote write_files
+    write_files:
+      - encoding: b64
+        content: ${data.local_file.cloud_provider_config.content_base64}
+        owner: root:root
+        path: /etc/kubernetes/cloud-config
+        permission: '0644'
+    ${indent(4, data.local_file.cloud_init.content)}
+    packages:
+      - qemu-guest-agent
+    runcmd:
+      - - systemctl
+        - enable
+        - '--now'
+        - qemu-guest-agent.service
+    EOF
+  }
+}
+
+# Create a new rancher2 machine config v2 using harvester node_driver
+resource "rancher2_machine_config_v2" "worker" {
+  generate_name = "worker"
   harvester_config {
     vm_namespace = "default"
     cpu_count = "2"
